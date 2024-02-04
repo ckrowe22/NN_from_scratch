e68456edb6f98756d0e65c4da1ee43de83819367
mrericreed <reederic@foothill.edu>
2024-02-04 15:54:17 -0800

    update MLN tests v2

--- test_MultiLinkNode.py
+++ test_MultiLinkNode.py
@@ -27,8 +27,11 @@ def test_MultiLinkNode():
 
 
 def test_MultiLinkNode_is_ABC():
-    with pytest.raises(TypeError):
+    try:
         Neurode.MultiLinkNode()
+        pytest.fail("Is MultiLinkNode an Abstract Base Class?")
+    except TypeError:
+        pass
 
 
 def test_Side(implemented_MultiLinkNode):
@@ -52,7 +55,7 @@ def test_reporting_nodes(implemented_MultiLinkNode):
         my_node._reporting_nodes[my_node.Side.UPSTREAM]
         my_node._reporting_nodes[my_node.Side.DOWNSTREAM]
     except:
-        pytest.fail("Does _reporting_nodes have an entry for UPSTREAM"
+        pytest.fail("Does _reporting_nodes have an entry for UPSTREAM "
                     "and DOWNSTREAM?")
     assert my_node._reporting_nodes[my_node.Side.UPSTREAM] \
         == 0, \
@@ -71,7 +74,7 @@ def test_initial_reference_value(implemented_MultiLinkNode):
         my_node._reference_value[my_node.Side.UPSTREAM]
         my_node._reference_value[my_node.Side.DOWNSTREAM]
     except:
-        pytest.fail("Does _reference_value have an entry for UPSTREAM"
+        pytest.fail("Does _reference_value have an entry for UPSTREAM "
                     "and DOWNSTREAM?")
     assert my_node._reference_value[my_node.Side.UPSTREAM] \
            == 0, \
@@ -89,20 +92,20 @@ def test_copy_vs_deepcopy(implemented_MultiLinkNode):
     node_list = [item_a, item_b]
     my_node.reset_neighbors(node_list, my_node.Side.UPSTREAM)
     assert my_node._neighbors[my_node.Side.UPSTREAM] is not node_list, \
-        ("After calling reset_neighbors, the _neighbors list is the same"
-         "object as the list that was passed into the function. This gives"
+        ("After calling reset_neighbors, the _neighbors list is the same "
+         "object as the list that was passed into the function. This gives "
          "the client access to _neighbors that it should not have")
     assert my_node._neighbors[my_node.Side.UPSTREAM][0] is item_a, \
         ("After calling reset_neighbors, the nodes in _neighbors should "
          "be the same objects as the nodes that were passed in (not "
          "copies)")
     assert my_node._neighbors[my_node.Side.DOWNSTREAM] == [], \
-        ("After calling reset_neighbors with UPSTREAM data, DOWNSTREAM"
+        ("After calling reset_neighbors with UPSTREAM data, DOWNSTREAM "
          "was changed")
     my_node.reset_neighbors(node_list, my_node.Side.DOWNSTREAM)
     assert my_node._neighbors[my_node.Side.DOWNSTREAM] is not node_list, \
-        ("After calling reset_neighbors, the _neighbors list is the same"
-         "object as the list that was passed into the function. This gives"
+        ("After calling reset_neighbors, the _neighbors list is the same "
+         "object as the list that was passed into the function. This gives "
          "the client access to _neighbors that it should not have")
     assert my_node._neighbors[my_node.Side.DOWNSTREAM][0] is item_a, \
         ("After calling reset_neighbors, the nodes in _neighbors should "
@@ -144,7 +147,7 @@ def test_neighbors_reset_before_loading_new_nodes(implemented_MultiLinkNode):
     my_node.reset_neighbors(node_list_two, my_node.Side.UPSTREAM)
     assert item_a not in my_node._neighbors[my_node.Side.UPSTREAM]
     assert my_node._reference_value[my_node.Side.UPSTREAM] == 1, \
-        ("UPSTREAM _reference_value is incorrect after calling"
+        ("UPSTREAM _reference_value is incorrect after calling "
          "reset_neighbors a second time")
     assert my_node._reference_value[my_node.Side.DOWNSTREAM] == 0, \
         ("DOWNSTREAM _reference_value was changed after loading "
