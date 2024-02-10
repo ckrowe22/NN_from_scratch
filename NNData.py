0324f426498fe7f6bcdb27ac8f07841b16e056ef
ckrowe22 <ckrowe13@gmail.com>
2024-02-10 08:54:25 -0500

    *too many characters in the docstring*

--- NNData.py
+++ NNData.py
@@ -1,4 +1,4 @@
-"""This module contains classes and methods for handling neural network data."""
+"""This module contains classes and methods to handle NN data."""
 
 from enum import Enum
 import collections
