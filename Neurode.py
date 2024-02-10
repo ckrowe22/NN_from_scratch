e8120dc851433d728db67ae258c2a7a77190c124
ckrowe22 <ckrowe13@gmail.com>
2024-02-10 08:45:19 -0500

    I *think* it's done!

--- Neurode.py
+++ Neurode.py
@@ -16,33 +16,32 @@ class MultiLinkNode(ABC):
         """Initialize an instance of MultiLinkNode Class.
         Parameters: None.
         """
-        self._reporting_nodes = {self.Side.UPSTREAM: 0, self.Side.DOWNSTREAM: 1}
-        self._reference_value = {self.Side.UPSTREAM: 0, self.Side.DOWNSTREAM: 1}
+        self._reporting_nodes = {self.Side.UPSTREAM: 0, self.Side.DOWNSTREAM: 0}
+        self._reference_value = {self.Side.UPSTREAM: 0, self.Side.DOWNSTREAM: 0}
         self._neighbors = {self.Side.UPSTREAM: [], self.Side.DOWNSTREAM: []}
 
     def __str__(self):
         """String representation of the node in context."""
-        return f'Node ID: {self._reference_value} ' \
-               f'\nUpstream Node IDs: {self._neighbors[self.Side.UPSTREAM]} ' \
-               f'\nDownstream Node IDs: {self._neighbors[self.Side.DOWNSTREAM]}'
+        return f'Node ID: {id(self._reference_value)} ' \
+               f'\nUpstream Node IDs: {id(self._neighbors[self.Side.UPSTREAM])} ' \
+               f'\nDownstream Node IDs: {id(self._neighbors[self.Side.DOWNSTREAM])}'
 
     @abstractmethod
     def _process_new_neighbor(self, node: MultiLinkNode, side: Side):
-        """..."""
+        """Creating abstract method to process new neighbor."""
         pass
 
     def reset_neighbors(self, nodes: list, side: Side):
-        """..."""
+        """Reset neighbor nodes."""
         self._neighbors[side] = []
+        self._neighbors[side] = nodes[:]  # copy of nodes
+        num_nodes = len(nodes)
+        if num_nodes > 0:
+            self._reference_value[side] = 2 ** num_nodes - 1
+        else:
+            self._reference_value[side] = 0
         for node in nodes:
-            self._neighbors[side].append(node)
             self._process_new_neighbor(node, side)
-
-        # assignment, copy, or deepcopy.
-        # The client could modify and reuse the nodes list, which has the potential to
-        # corrupt self._neighbors.
-        # We don't want to create new MultiLinkNodes, we want references to nodes
-        # that already exist.
 
 
 class Neurode(MultiLinkNode, ABC):
@@ -51,7 +50,7 @@ class Neurode(MultiLinkNode, ABC):
     _learning_rate = .05
 
     def __init__(self):
-        """Initialize an instance of MultiLinkNode Class.
+        """Initialize an instance of Neurode Class.
         Parameters: None.
         """
         self._value = 0
@@ -60,36 +59,34 @@ class Neurode(MultiLinkNode, ABC):
 
     @property
     def learning_rate(self):
-        # property decorator .05
+        """Getter for learning rate."""
         return self._learning_rate
 
     @learning_rate.setter
     def learning_rate(self, value: float):
+        """Setter for learning rate."""
         self._learning_rate = value
 
-        # This is actually a pair of methods, a setter and a getter coded
-        # with the @ property decorator.They should get or set the class
-        # attribute that you defined above.In your testing, make sure that this
-        # truly behaves as a class attribute.In other words, changing the
-        # learning rate for one object of the class should change the learning rate
-        # for all objects of the class.
-
-    def _process_new_neighbor(self, node: MultiLinkNode, side: ABC.Side):
-        if side == ABC.Side.UPSTREAM:
+    def _process_new_neighbor(self, node: MultiLinkNode, side: MultiLinkNode.Side):
+        """Process new neighbors for UPSTREAM nodes."""
+        if side == MultiLinkNode.Side.UPSTREAM:
             self._weights[node] = random.random()
 
-    def _check_in(self, node: Neurode, side: ABC.Side):
+    def _check_in(self, node: Neurode, side: MultiLinkNode.Side):
+        """"Checks if neighboring nodes have reported."""
         index = self._neighbors[side].index(node)
-        self._reporting_nodes[side] = node[index]
-        if self._reporting_nodes[side] == self._reference_value:
-            self._reporting_nodes = 0
+        self._reporting_nodes[side] |= 1 << index  # try bitwise or?
+        if self._reporting_nodes[side] == self._reference_value[side]:
+            self._reporting_nodes[side] = 0
             return True
         else:
             return False
 
     def get_weight(self, node: Neurode):
+        """Get weights for nodes."""
         return self._weights.get(node, None)
 
     @property
     def value(self):
+        """Returns value."""
         return self._value
