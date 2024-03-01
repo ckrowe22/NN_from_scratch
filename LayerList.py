412e1415798f5b39bb39581069b793998dc674c0
ckrowe22 <ckrowe13@gmail.com>
2024-02-29 20:05:06 -0500

    testing

--- LayerList.py
+++ LayerList.py
@@ -12,18 +12,19 @@ class LayerList(DoublyLinkedList):
         self.output_layer = self._create_layer(outputs, neurode_type)
         self.add_to_head(self.input_layer)
         self.add_after_current(self.output_layer)
-        self.link_layers(self.input_layer, self.output_layer)
+        #self._link_layers(self.input_layer, self.output_layer)
 
     def _create_layer(self, num_nodes: int, neurode_type: type(Neurode)):
         new_layer_list = [neurode_type() for x in range(num_nodes)]
         return new_layer_list
 
-    def link_layers(self, input_list, output_list):
+    def _link_layers(self, input_list, output_list):
         for upstream_node in input_list:
             upstream_node.reset_neighbors(output_list, Neurode.Side.DOWNSTREAM)
         for downstream_node in output_list:
-            # downstream_node._process_new_neighbor(downstream_node, Neurode.Side.UPSTREAM)
             downstream_node.reset_neighbors(input_list, Neurode.Side.UPSTREAM)
+
+
 
     def add_layer(self, num_nodes: int):
         if self._curr == self._tail:
@@ -31,16 +32,18 @@ class LayerList(DoublyLinkedList):
         new_layer = self._create_layer(num_nodes, self._neurode_type)
         self.add_after_current(new_layer)
         if self._curr == self._head:
-            self.link_layers(self._curr.data, new_layer)
-            self.link_layers(new_layer, self._curr.next.data)
+            self._link_layers(self._curr.data, new_layer)
+            self._link_layers(new_layer, self._curr.next.data)
         else:
-            self.link_layers(self._curr.data, new_layer)
-            self.link_layers(new_layer, self._curr.prev.data)
+            self._link_layers(self._curr.data, new_layer)
+            self._link_layers(new_layer, self._curr.prev.data)
 
     def remove_layer(self):
         if self._curr == self._tail:
             raise IndexError("Cannot remove output layer.")
-        self.remove(self._curr)
+        self.remove_after_current()
+        self._link_layers(self._curr.data, self._curr.next.data)
+
 
         # remove layer after the current layer
         # raise index error if attempting to remove (tail) output layer
